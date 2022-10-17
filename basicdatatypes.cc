a8b8974c94366eb0cb611141fa9823440d2e09fe
Nitin srivastava <nitinsri2954@gmail.com>
2022-10-17 19:15:41 +0530

    Create basicdatatypes.cc

---
+++ basicdatatypes.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdio>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    int a; long b;char c;float d;double e;
+    cin>>a>>b>>c>>d>>e;
+    cout<<a<<endl<<b<<endl<<c<<endl<<setprecision(7)<<d<<endl<<setprecision(17)<<e<<endl;
+    return 0;
+}
